8739d8b8130d6206002e570be0b3a8d8faaef1d8
iqbalthex <73935864+iqbalthex@users.noreply.github.com>
2022-12-04 21:32:34 +0700

    Add files via upload

---
+++ case-1/index.html
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Simple Map</title>
+	<meta name="viewport" content="initial-scale=1.0">
+	<meta charset="utf-8">
+	<script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="./style.css" />
+</head>
+<body>
+	<main class="container-fluid">
+		<h1 class="text-center mt-1">Tambah Lokasi Favorit</h1>
+		<div id="map-group" class="overflow-hidden">
+			<div id="map-menu" class="overflow-scroll">
+				<h3 class="text-center position-fixed">Daftar Lokasi</h3>
+				<div class="list-group">
+					<a class="list-group-item">.</a>
+					<a class="list-group-item" data-lat="0.475001" data-lng="101.380812">UPT Bahasa</a>
+					<a class="list-group-item" data-lat="0.477772" data-lng="101.379622">UPT TIK</a>
+					<a class="list-group-item" data-lat="0.478153" data-lng="101.380234">Gedung Arsip</a>
+					<a class="list-group-item" data-lat="0.476818" data-lng="101.379758">Perpustakaan</a>
+					<a class="list-group-item" data-lat="0.478577" data-lng="101.378587">Fakultas Pertanian</a>
+					<a class="list-group-item" data-lat="0.478491" data-lng="101.380996">Fakultas Perikanan dan Kelautan</a>
+					<a class="list-group-item" data-lat="0.477544" data-lng="101.377212">Fakultas Matematika dan Ilmu Pengetahuan</a>
+					<a class="list-group-item" data-lat="0.474385" data-lng="101.376335">Fakultas Ekonomi dan Bisnis</a>
+					<a class="list-group-item" data-lat="0.473551" data-lng="101.378677">Fakultas Ilmu Sosial dan Politik</a>
+				</div>
+			</div>
+			<div id="map"></div>
+		</div>
+	</main>
+
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg&callback=initMap&v=weekly" defer></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+<script src="./script.js"></script>
+</body>
+</html>
---
+++ case-1/script.js
@@ -0,0 +1,151 @@
+// mengambil elemen-elemen (daftar lokasi)
+const places = document.querySelectorAll("a[data-lat][data-lng]");
+
+// posisi awal
+const pusat = { lat: 0.476818, lng: 101.379758 };
+
+// inisiasi objek
+let infoWindow, trafficLayer, map, marker;
+let favItems, favCollapsed = false;
+let favList = [];
+
+if(!localStorage.getItem("myMap")){
+	const datas = JSON.stringify({ favPlaces:[] });
+	localStorage.setItem("myMap", datas);
+}
+
+let datas = JSON.parse(localStorage.getItem("myMap"));
+
+
+// fungsi untuk memunculkan peta
+window.initMap = function(){
+	// membuat peta
+	map = new google.maps.Map(document.getElementById("map"), {
+		center: pusat, zoom: 18, mapTypeId: "terrain", disableDefaultUI: true,
+	});
+
+	map.addListener('click', e => {
+		const lat = e.latLng.lat();
+		const lng = e.latLng.lng();
+
+		const content = `<div id="place-info" class="p-3">
+			<button class="btn btn-primary mt-2 mb-1"
+				data-lat="${lat}"
+				data-lng="${lng}"
+				data-bs-toggle="modal"
+				data-bs-target="#modal"
+				onclick="addFav()">Tambah ke favorit</button>
+		</div>`;
+		updateInfo({ lat: lat + 0.0002, lng }, content);
+	});
+
+	infoWindow = new google.maps.InfoWindow({ content: "", });
+	marker = new google.maps.Marker({map});
+
+	// memberikan fungsi ketika list tempat diklik
+	places.forEach(place => {
+		place.onclick = e => {
+			const name = place.innerText;
+			let { lat, lng } = e.target.dataset; // mengambil nilai di elemen html
+			lat = parseFloat(lat);
+			lng = parseFloat(lng);
+			map.setCenter({ lat, lng });
+
+			const placeNames = favList.map(e => e.name);
+
+			let content = `<div id="place-info" class="p-3">
+				<h5>${place.innerText}</h5>
+				<p class="mt-1"><span class="ps-2">Lintang</span><span>: ${lat}</span></p>
+				<p class="mb-1"><span class="ps-2">Bujur</span><span>: ${lng}</span></p>
+			`;
+
+			const inList = favList.filter(e => e.name === name).length == 1;
+			if(inList){
+				content += `<button id="delBtn"
+					class="btn btn-primary mt-2 mb-1"
+					data-name="${name}"
+					onclick="delFav(this)">Hapus dari favorit
+				</button>`;
+			} else {
+				content += `<button id="addBtn"
+					class="btn btn-primary mt-2 mb-1"
+					data-lat="${lat}"
+					data-lng="${lng}"
+					data-name="${name}"
+					onclick="addFav(this)">Tambah ke favorit
+				</button>`;
+			}
+
+			content += '</div>';
+
+			updateInfo({ lat: lat + 0.0002, lng }, content);
+			marker.setPosition({ lat, lng });
+		};
+	});
+
+	const favPlace = document.createElement("div");
+	favPlace.classList.add("position-absolute");
+	favPlace.classList.add("fs-5");
+	favPlace.classList.add("mt-2");
+	favPlace.classList.add("me-2");
+	favPlace.innerHTML = `<div>
+		<button id="favToggler" class="btn bg-warning m-0" onclick="favToggle">Lokasi favorit</button>
+		<div class="position-absolute" style="right: 0">
+			<div class="list-group position-relative bg-info"></div>
+		</div>
+	</div>`;
+
+	map.controls[google.maps.ControlPosition.TOP_RIGHT].push(favPlace);
+
+	setTimeout(() => {
+		favItems = document.querySelector("#favToggler + div > div.list-group");
+		document.getElementById("favToggler").onclick = favToggle;
+	}, 2000);
+};
+
+
+// fungsi untuk mengubah infoWindow
+function updateInfo(pos, content){
+	infoWindow.setPosition(pos);
+	infoWindow.setContent(content);
+	infoWindow.open(map);
+}
+
+
+// fungsi untuk memperbarui daftar tempat favorit
+function updateList(){
+	const items = favList.filter((v,i,a) => a.findIndex(v2 => ['name'].every(k => v2[k] === v[k])) === i);
+	datas.favPlaces = items;
+	localStorage.setItem("myMap", JSON.stringify(datas));
+
+	let content = '';
+	items.forEach(e => {
+		content += `<a class="list-group-item bg-info px-2 py-0">${e.name}</a>`;
+	});
+	favItems.innerHTML = content;
+}
+
+
+// fungsi untuk memunculkan/menyembunyikan daftar tempat favorit
+function favToggle(){
+	favCollapsed = !favCollapsed;
+	favItems.style.opacity = favCollapsed ? '0' : '1';
+}
+
+
+// menambahkan tempat ke daftar favorit
+function addFav(el){
+	const { name, lat, lng } = el.dataset;
+	favList.push({ name, lat, lng });
+
+	updateList();
+}
+
+
+// menghapus tempat dari daftar favorit
+function delFav(el){
+	const { name, lat, lng } = el.dataset;
+	favList = favList.filter(e => e.name !== name);
+
+	updateList();
+}
